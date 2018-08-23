128e56874508f6531f2c61205c4ecd6abfa2dbea
Leo <leonoragilmour@sky.com>
2018-08-23 16:31:58 +0100

    init

---
+++ readme.md
@@ -0,0 +1,2 @@
+# WP Shortlist Media Family Plugin
+A plugin for custom widgets and fields that the Family site needs
---
+++ wp-shortlist-media-family-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: WP Shortlist Media Family Plugin
+* Plugin URI: http://github.com/shortlist-digital/wp-shortlist-media-family-plugin
+* Description: A plugin for custom widgets and fields that the Family site needs
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class WpShortlistMediaFamilyPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new WpShortlistMediaFamilyPlugin();
